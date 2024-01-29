373abb4ae9b1ebd35b9f0121f79ecaf837e0f5fc
Syeed-MD-Talha <62591318+Syeed-MD-Talha@users.noreply.github.com>
2024-01-29 19:12:08 +0600

    done

---
+++ Ajax_tutorial/basicAjax/Ajax_tutorial/code.js
@@ -0,0 +1,16 @@
+
+
+function loadData() {
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = function () {
+        const container = document.getElementById("demo");
+        demo.innerHTML = xhr.responseText;
+    }
+
+    xhr.open('GET', 'dummy.txt');
+
+    xhr.send();
+
+}
